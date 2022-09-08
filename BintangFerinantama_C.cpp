c001f7b47396ba80fc88479b6c7a19a143f4cb92
BintangFerinantama <112613634+BintangFerinantama@users.noreply.github.com>
2022-09-08 14:31:11 +0700

    Add files via upload

---
+++ BintangFerinantama_C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int a, b, c, d, e;
+ 
+  a = 8 + 4;
+  b = 8 - 4;
+  c = 8 * 4;
+  d = 8 / 4;
+  e = 8 % 4;
+ 
+  cout << "Isi variabel a: " << a << endl;
+  cout << "Isi variabel b: " << b << endl;
+  cout << "Isi variabel c: " << c << endl;
+  cout << "Isi variabel d: " << d << endl;
+  cout << "Isi variabel e: " << e << endl;
+ 
+  return 0;
+}
